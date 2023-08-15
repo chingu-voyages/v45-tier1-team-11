8c910d28d07b7e4dc4db65e303e45fb68a86c0b9
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-08-15 17:56:30 +0100

    Add mass component to search

--- voyage-project-tier1-fireball/index.js
+++ voyage-project-tier1-fireball/index.js
@@ -11,6 +11,28 @@ async function search(searchParams) {
             meteorites = meteorites.filter(meteorite =>
                 meteorite[parameter] ? meteorite[parameter].substring(0, 4) === value : false
             );
+        } else if (parameter == 'mass') {
+            if (value === 'very low') {
+                meteorites = meteorites.filter(meteorite =>
+                    meteorite[parameter] ? parseInt(meteorite[parameter]) < 100 : false
+                );
+            } else if (value === 'low') {
+                meteorites = meteorites.filter(meteorite =>
+                    meteorite[parameter] ? (parseInt(meteorite[parameter]) >= 100 && parseInt(meteorite[parameter]) < 1000) : false
+                );
+            } else if (value === 'medium') {
+                meteorites = meteorites.filter(meteorite =>
+                    meteorite[parameter] ? (parseInt(meteorite[parameter]) >= 1000 && parseInt(meteorite[parameter]) < 10000) : false
+                );
+            } else if (value === 'high') {
+                meteorites = meteorites.filter(meteorite =>
+                    meteorite[parameter] ? (parseInt(meteorite[parameter]) >= 10000 && parseInt(meteorite[parameter]) < 100000) : false
+                );
+            } else if (value === 'very high') {
+                meteorites = meteorites.filter(meteorite =>
+                    meteorite[parameter] ? parseInt(meteorite[parameter]) >= 100000 : false
+                );
+            }
         } else {
             meteorites = meteorites.filter(meteorite =>
                 meteorite[parameter] ? meteorite[parameter] === value : false
@@ -28,7 +50,7 @@ async function search(searchParams) {
 const button = document.getElementById('searchButton');
 button.addEventListener('click', async () => {
     const searchParams = {
-        year: '1880',
+        mass: 'very low',
     };
     await search(searchParams);
 });
