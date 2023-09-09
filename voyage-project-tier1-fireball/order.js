2435a6a2e43db4a4a118e65dd839b0b241106454
amavicpos <100805638+amavicpos@users.noreply.github.com>
2023-09-09 16:01:21 +0100

    Add asc desc reordering for each column

--- voyage-project-tier1-fireball/order.js
+++ voyage-project-tier1-fireball/order.js
@@ -1,18 +1,50 @@
+const columnOrder = {
+    "name-col": "asc",
+    "comp-col": "asc",
+    "year-col": "asc",
+    "mass-col": "asc"
+  };
+
 const nameCol = document.getElementById("name-col");
 const compCol = document.getElementById("comp-col");
 const yearCol = document.getElementById("year-col");
 const massCol = document.getElementById("mass-col");
+
 nameCol.addEventListener("click", () => {
-    reorderTable(0, "asc");
+    if (columnOrder["name-col"] == "asc") {
+        reorderTable(0, "asc");
+        columnOrder["name-col"] = "desc";
+    } else {
+        reorderTable(0, "desc");
+        columnOrder["name-col"] = "asc";
+    }
 });
 compCol.addEventListener("click", () => {
-    reorderTable(1, "asc");
+    if (columnOrder["comp-col"] == "asc") {
+        reorderTable(1, "asc");
+        columnOrder["comp-col"] = "desc";
+    } else {
+        reorderTable(1, "desc");
+        columnOrder["comp-col"] = "asc";
+    }
 });
 yearCol.addEventListener("click", () => {
-    reorderTable(2, "asc");
+    if (columnOrder["year-col"] == "asc") {
+        reorderTable(2, "asc");
+        columnOrder["year-col"] = "desc";
+    } else {
+        reorderTable(2, "desc");
+        columnOrder["year-col"] = "asc";
+    }
 });
 massCol.addEventListener("click", () => {
-    reorderTable(3, "asc");
+    if (columnOrder["mass-col"] == "asc") {
+        reorderTable(3, "asc");
+        columnOrder["mass-col"] = "desc";
+    } else {
+        reorderTable(3, "desc");
+        columnOrder["mass-col"] = "asc";
+    }
 });
 
 // Function to reorder table
